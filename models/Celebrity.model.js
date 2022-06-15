fa129c6e13bf12d427f86134a5ab611a78cfb439
CheloCr <xchell_cruz@hotmail.com>
2022-06-15 12:57:21 -0500

    iteration 1 and 2

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,22 @@
 //  Add your code here
+//todo IMPOTAMOS MONGOOSE
+const {Schema,model, SchemaType, default: mongoose} = require("mongoose")
+
+
+//todo CREAMOS SCHEMA
+
+const celebritySchema = new Schema ({
+    name: String,
+    occupation:String,
+    catchPhrase:String
+})
+
+//todo SE CREA MODELO
+const Celebrity = mongoose.model('Celebrities',celebritySchema)
+
+//todo EXPORTAMOS MODELO
+module.exports = Celebrity
+
+
+//? version corta para crear y exporta
+//? model.exports = model("Celebrities",celebritySchema)
